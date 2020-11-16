7780a39fc133f8505b7bd1e4a77a39cf50067545
Zachary Invergo <zinvergo@gmail.com>
2020-11-16 14:48:07 -0600

    rebuild after branch loss

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, redirect, url_for
-
+from flask import Flask, redirect, url_for, render_template
+​
+​
 app = Flask(__name__)
+​
+​
+
 
 @app.route('/')
 def home():
-    return "Hello! This is the main page."
+    return render_template('index.html', name="Cory")
 
-@app.route('/<name>')
-def user(name):
-    return f'Hello, {name}!'
 
-@app.route('/admin')
-def admin():
-    return redirect(url_for('home'))
+​
+​
 
-# @app.route('/admin')
-# def admin():
-#     return redirect(url_for('user', name="jim"))
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+​
+​
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+​
+​
 if __name__ == "__main__":
     app.run(debug=True)
